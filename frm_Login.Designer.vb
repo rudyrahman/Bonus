0ebe5e04948faf11509797041e129f503c886f5e
PTANSD <rudyrahman@windowslive.com>
2015-05-06 10:41:36 +0700

    sync sukma

--- frm_Login.Designer.vb
+++ frm_Login.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frm_Login
+Partial Class frm_login
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,92 +22,92 @@ Partial Class frm_Login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Login))
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.txt_username = New System.Windows.Forms.TextBox()
-        Me.txt_password = New System.Windows.Forms.TextBox()
-        Me.btn_exit = New System.Windows.Forms.Button()
-        Me.btn_login = New System.Windows.Forms.Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_login))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btn_login = New System.Windows.Forms.Button()
+        Me.btn_exit = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt_password = New System.Windows.Forms.TextBox()
+        Me.txt_username = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(248, 77)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(55, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Username"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(248, 148)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Password"
-        '
-        'txt_username
-        '
-        Me.txt_username.Location = New System.Drawing.Point(251, 108)
-        Me.txt_username.Name = "txt_username"
-        Me.txt_username.Size = New System.Drawing.Size(221, 20)
-        Me.txt_username.TabIndex = 2
-        '
-        'txt_password
-        '
-        Me.txt_password.Location = New System.Drawing.Point(251, 185)
-        Me.txt_password.Name = "txt_password"
-        Me.txt_password.Size = New System.Drawing.Size(221, 20)
-        Me.txt_password.TabIndex = 3
-        '
-        'btn_exit
-        '
-        Me.btn_exit.Location = New System.Drawing.Point(478, 251)
-        Me.btn_exit.Name = "btn_exit"
-        Me.btn_exit.Size = New System.Drawing.Size(75, 23)
-        Me.btn_exit.TabIndex = 5
-        Me.btn_exit.Text = "Exit"
-        Me.btn_exit.UseVisualStyleBackColor = True
-        '
-        'btn_login
-        '
-        Me.btn_login.Location = New System.Drawing.Point(397, 251)
-        Me.btn_login.Name = "btn_login"
-        Me.btn_login.Size = New System.Drawing.Size(75, 23)
-        Me.btn_login.TabIndex = 6
-        Me.btn_login.Text = "Login"
-        Me.btn_login.UseVisualStyleBackColor = True
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(12, 51)
+        Me.PictureBox1.Location = New System.Drawing.Point(22, 12)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(230, 206)
-        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.Size = New System.Drawing.Size(154, 154)
+        Me.PictureBox1.TabIndex = 23
         Me.PictureBox1.TabStop = False
+        '
+        'btn_login
+        '
+        Me.btn_login.Location = New System.Drawing.Point(293, 196)
+        Me.btn_login.Name = "btn_login"
+        Me.btn_login.Size = New System.Drawing.Size(75, 23)
+        Me.btn_login.TabIndex = 22
+        Me.btn_login.Text = "Login"
+        Me.btn_login.UseVisualStyleBackColor = True
+        '
+        'btn_exit
+        '
+        Me.btn_exit.Location = New System.Drawing.Point(390, 196)
+        Me.btn_exit.Name = "btn_exit"
+        Me.btn_exit.Size = New System.Drawing.Size(75, 23)
+        Me.btn_exit.TabIndex = 21
+        Me.btn_exit.Text = "Exit"
+        Me.btn_exit.UseVisualStyleBackColor = True
         '
         'Button1
         '
         Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
-        Me.Button1.Location = New System.Drawing.Point(478, 174)
+        Me.Button1.Location = New System.Drawing.Point(432, 119)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(81, 58)
-        Me.Button1.TabIndex = 4
+        Me.Button1.Size = New System.Drawing.Size(52, 47)
+        Me.Button1.TabIndex = 20
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'frm_Login
+        'txt_password
+        '
+        Me.txt_password.Location = New System.Drawing.Point(194, 133)
+        Me.txt_password.Name = "txt_password"
+        Me.txt_password.Size = New System.Drawing.Size(221, 20)
+        Me.txt_password.TabIndex = 19
+        '
+        'txt_username
+        '
+        Me.txt_username.Location = New System.Drawing.Point(194, 50)
+        Me.txt_username.Name = "txt_username"
+        Me.txt_username.Size = New System.Drawing.Size(221, 20)
+        Me.txt_username.TabIndex = 18
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(193, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(191, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Username"
+        '
+        'frm_login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(571, 311)
+        Me.ClientSize = New System.Drawing.Size(491, 242)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.btn_login)
         Me.Controls.Add(Me.btn_exit)
@@ -116,19 +116,19 @@ Partial Class frm_Login
         Me.Controls.Add(Me.txt_username)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Name = "frm_Login"
-        Me.Text = "frm_Login"
+        Me.Name = "frm_login"
+        Me.Text = "frm_login"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents txt_username As System.Windows.Forms.TextBox
-    Friend WithEvents txt_password As System.Windows.Forms.TextBox
-    Friend WithEvents btn_exit As System.Windows.Forms.Button
-    Friend WithEvents btn_login As System.Windows.Forms.Button
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btn_login As System.Windows.Forms.Button
+    Friend WithEvents btn_exit As System.Windows.Forms.Button
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txt_password As System.Windows.Forms.TextBox
+    Friend WithEvents txt_username As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
