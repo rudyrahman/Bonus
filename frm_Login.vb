17c90800ecdf0505ed81a8d814a5ff0ba2306dac
SukmaPerdana <sukmaperdana@rocketmail.com>
2015-05-06 10:11:21 +0700

    frm_login

--- frm_Login.vb
+++ frm_Login.vb
@@ -1,8 +1,9 @@
 ﻿Imports ADODB
-Public Class frm_Login
+
+Public Class frm_login
     Dim cn As New ADODB.Connection
     Dim rs As New ADODB.Recordset
-    Private Sub frm_Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub frm_login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Try
             Me.MinimizeBox = False
             Me.MaximizeBox = False
@@ -24,7 +25,25 @@ Public Class frm_Login
     End Sub
 
     Private Sub btn_login_Click(sender As Object, e As EventArgs) Handles btn_login.Click
-
+        Try
+            If txt_username.Text = "" Or txt_password.Text = "" Then
+                MsgBox("Silahkan isi Username dan Password", vbInformation, "Login")
+            Else
+                rs = New ADODB.Recordset
+                rs.Open("SELECT * FROM [AN_SUMATRA].[dbo].[SY_tb_appsaccounts] WHERE [username]='" & txt_username.Text & "' and [password]='" & txt_password.Text & "'", cn, CursorTypeEnum.adOpenKeyset)
+                If Not rs.EOF Then
+                    MsgBox("Selamat Anda Berhasil Login", vbInformation, "Pemberitahuan")
+                    Me.Close()
+                    cn.Close()
+                Else
+                    MsgBox("Maaf User atau password salah", vbInformation, "Pemberitahuan")
+                    txt_username.Text = ""
+                    txt_password.Text = ""
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
     End Sub
 
     Private Sub btn_exit_Click(sender As Object, e As EventArgs) Handles btn_exit.Click
