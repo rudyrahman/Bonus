65d5df4be85ff3b00b04d53a10d01be6e0890b97
PTANSD <rudyrahman@windowslive.com>
2015-05-06 10:26:12 +0700

    Fix Error on Riza Project

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -21,18 +21,18 @@ Namespace My
     '
     Partial Friend Class MyApplication
         
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Public Sub New()
-            MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-            Me.IsSingleInstance = false
-            Me.EnableVisualStyles = true
-            Me.SaveMySettingsOnExit = true
-            Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
-        End Sub
-        
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
-        Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Bonus_System.frm_Systeck
-        End Sub
+        '<Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        'Public Sub New()
+        '    MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
+        '    Me.IsSingleInstance = false
+        '    Me.EnableVisualStyles = true
+        '    Me.SaveMySettingsOnExit = true
+        '    Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
+        'End Sub
+
+        '<Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        ''Protected Overrides Sub OnCreateMainForm()
+        ''    Me.MainForm = Global.Bonus_System.frm_Systeck
+        ''End Sub
     End Class
 End Namespace
