4a3a413034a3c86bbe86bc16084e358a1f7806f1
riza <rizayusuf@rocketmail.com>
2015-05-11 16:59:08 +0700

    frm_UserManagement 10 %

--- ANapps.vb
+++ ANapps.vb
@@ -6,4 +6,12 @@ Public Class ANapps
         Me.Visible = True
         frm_login.ShowDialog()
     End Sub
+
+    Private Sub mnu_UserManagement_Click(sender As Object, e As EventArgs) Handles mnu_UserManagement.Click
+        frm_UserManagement.Show()
+    End Sub
+
+    Private Sub mnu_AddNewUser_Click(sender As Object, e As EventArgs) Handles mnu_AddNewUser.Click
+
+    End Sub
 End Class
