f5a95c74612ab09d5b2a8ce2cab18bfe65b89ffd
riza <rizayusuf@rocketmail.com>
2015-05-29 09:03:04 +0700

    form

---
+++ frm_ImportDataAllowence.designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_ImportDataAllowence
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_Cancel = New System.Windows.Forms.Button()
+        Me.btn_Save = New System.Windows.Forms.Button()
+        Me.btn_ImportFromExcel = New System.Windows.Forms.Button()
+        Me.dgv_ImportDataAllowence = New System.Windows.Forms.DataGridView()
+        Me.cbo_Devision = New System.Windows.Forms.ComboBox()
+        Me.cbo_year = New System.Windows.Forms.ComboBox()
+        Me.cbo_Month = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.dgv_ImportDataAllowence, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn_Cancel
+        '
+        Me.btn_Cancel.BackColor = System.Drawing.Color.Red
+        Me.btn_Cancel.Location = New System.Drawing.Point(145, 511)
+        Me.btn_Cancel.Name = "btn_Cancel"
+        Me.btn_Cancel.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Cancel.TabIndex = 19
+        Me.btn_Cancel.Text = "Cancel"
+        Me.btn_Cancel.UseVisualStyleBackColor = False
+        '
+        'btn_Save
+        '
+        Me.btn_Save.BackColor = System.Drawing.Color.Green
+        Me.btn_Save.Location = New System.Drawing.Point(55, 511)
+        Me.btn_Save.Name = "btn_Save"
+        Me.btn_Save.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Save.TabIndex = 18
+        Me.btn_Save.Text = "Save"
+        Me.btn_Save.UseVisualStyleBackColor = False
+        '
+        'btn_ImportFromExcel
+        '
+        Me.btn_ImportFromExcel.Location = New System.Drawing.Point(603, 133)
+        Me.btn_ImportFromExcel.Name = "btn_ImportFromExcel"
+        Me.btn_ImportFromExcel.Size = New System.Drawing.Size(122, 23)
+        Me.btn_ImportFromExcel.TabIndex = 17
+        Me.btn_ImportFromExcel.Text = "Import From Excel"
+        Me.btn_ImportFromExcel.UseVisualStyleBackColor = True
+        '
+        'dgv_ImportDataAllowence
+        '
+        Me.dgv_ImportDataAllowence.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.dgv_ImportDataAllowence.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_ImportDataAllowence.Location = New System.Drawing.Point(55, 173)
+        Me.dgv_ImportDataAllowence.Name = "dgv_ImportDataAllowence"
+        Me.dgv_ImportDataAllowence.Size = New System.Drawing.Size(670, 320)
+        Me.dgv_ImportDataAllowence.TabIndex = 16
+        '
+        'cbo_Devision
+        '
+        Me.cbo_Devision.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbo_Devision.FormattingEnabled = True
+        Me.cbo_Devision.Location = New System.Drawing.Point(145, 83)
+        Me.cbo_Devision.Name = "cbo_Devision"
+        Me.cbo_Devision.Size = New System.Drawing.Size(193, 21)
+        Me.cbo_Devision.TabIndex = 15
+        '
+        'cbo_year
+        '
+        Me.cbo_year.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbo_year.FormattingEnabled = True
+        Me.cbo_year.Location = New System.Drawing.Point(145, 56)
+        Me.cbo_year.Name = "cbo_year"
+        Me.cbo_year.Size = New System.Drawing.Size(121, 21)
+        Me.cbo_year.TabIndex = 14
+        '
+        'cbo_Month
+        '
+        Me.cbo_Month.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbo_Month.FormattingEnabled = True
+        Me.cbo_Month.Location = New System.Drawing.Point(145, 29)
+        Me.cbo_Month.Name = "cbo_Month"
+        Me.cbo_Month.Size = New System.Drawing.Size(97, 21)
+        Me.cbo_Month.TabIndex = 13
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(52, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Devision :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Year :"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(52, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Month :"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'frm_ImportDataAllowence
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lavender
+        Me.ClientSize = New System.Drawing.Size(784, 562)
+        Me.Controls.Add(Me.btn_Cancel)
+        Me.Controls.Add(Me.btn_Save)
+        Me.Controls.Add(Me.btn_ImportFromExcel)
+        Me.Controls.Add(Me.dgv_ImportDataAllowence)
+        Me.Controls.Add(Me.cbo_Devision)
+        Me.Controls.Add(Me.cbo_year)
+        Me.Controls.Add(Me.cbo_Month)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frm_ImportDataAllowence"
+        Me.Text = "frm_ImportDataAllowence"
+        CType(Me.dgv_ImportDataAllowence, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_Cancel As System.Windows.Forms.Button
+    Friend WithEvents btn_Save As System.Windows.Forms.Button
+    Friend WithEvents btn_ImportFromExcel As System.Windows.Forms.Button
+    Friend WithEvents dgv_ImportDataAllowence As System.Windows.Forms.DataGridView
+    Friend WithEvents cbo_Devision As System.Windows.Forms.ComboBox
+    Friend WithEvents cbo_year As System.Windows.Forms.ComboBox
+    Friend WithEvents cbo_Month As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+End Class
---
+++ frm_ImportDataAllowence.vb
@@ -0,0 +1,178 @@
+﻿Imports ADODB
+Imports System.Data.OleDb
+Imports System.IO
+Imports System.Data
+Imports vbexcel = Microsoft.Office.Interop.Excel
+Public Class frm_ImportDataAllowence
+    Dim cn As New ADODB.Connection
+    Dim rs As New ADODB.Recordset
+
+    Public code, description As String
+    Public value As Integer
+    Public cmd = CreateObject("ADODB.Command")
+
+    Private Excel03ConString As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'"
+    Private Excel07ConString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'"
+
+    Private Sub frm_ImportDataAllowence_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Size = New Size(800, 600)
+        Me.MinimumSize = New Size(600, 400)
+        Try
+            cn.ConnectionString = "Provider=SQLNCLI11;Server=192.168.0.1;Database=AN_SUMATRA;Uid=itdevelopment;Pwd=itdevelopment2015"
+            cn.Open()
+            rs = cn.Execute("SELECT [month] FROM [AN_SUMATRA].[dbo].[TM_tb_month] ORDER BY [id] ASC")
+            If ((rs.EOF = False) And (rs.BOF = False)) = True Then
+                While Not rs.EOF
+                    cbo_Month.Items.Add(rs(0).Value.ToString)
+                    rs.MoveNext()
+                End While
+            End If
+
+            rs = cn.Execute("SELECT [year] FROM [AN_SUMATRA].[dbo].[TM_tb_year] ORDER BY [id] ASC")
+            If ((rs.EOF = False) And (rs.BOF = False)) = True Then
+                While Not rs.EOF
+                    cbo_year.Items.Add(rs(0).Value.ToString)
+                    rs.MoveNext()
+                End While
+            End If
+
+            rs = cn.Execute("SELECT [description] FROM [AN_SUMATRA].[dbo].[TM_tb_devision] ORDER BY [description] ASC")
+            If ((rs.EOF = False) And (rs.BOF = False)) = True Then
+                While Not rs.EOF
+                    cbo_Devision.Items.Add(rs(0).Value.ToString)
+                    rs.MoveNext()
+                End While
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+        cn.Close()
+    End Sub
+
+    Private Sub btn_ImportFromExcel_Click(sender As Object, e As EventArgs) Handles btn_ImportFromExcel.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Dim filePath As String = OpenFileDialog1.FileName
+        Dim extension As String = Path.GetExtension(filePath)
+        Dim conStr As String, sheetName As String
+
+        conStr = String.Empty
+        Select Case extension
+
+            Case ".xls"
+                'Excel 97-03
+                conStr = String.Format(Excel03ConString, filePath, 0)
+                Exit Select
+
+            Case ".xlsx"
+                'Excel 07
+                conStr = String.Format(Excel07ConString, filePath, 0)
+                Exit Select
+        End Select
+
+        'Get the name of the First Sheet.
+        Using con As New OleDbConnection(conStr)
+            Using cmd As New OleDbCommand()
+                cmd.Connection = con
+                con.Open()
+                Dim dtExcelSchema As DataTable = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, Nothing)
+                sheetName = dtExcelSchema.Rows(0)("TABLE_NAME").ToString()
+                con.Close()
+            End Using
+        End Using
+
+        'Read Data from the First Sheet.
+        Using con As New OleDbConnection(conStr)
+            Using cmd As New OleDbCommand()
+                Using oda As New OleDbDataAdapter()
+                    Dim dt As New DataTable()
+                    cmd.CommandText = (Convert.ToString("SELECT * From [") & sheetName) + "]"
+                    cmd.Connection = con
+                    con.Open()
+                    oda.SelectCommand = cmd
+                    oda.Fill(dt)
+                    con.Close()
+
+                    'Populate DataGridView.
+                    dgv_ImportDataAllowence.DataSource = dt
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Sub btn_Save_Click(sender As Object, e As EventArgs) Handles btn_Save.Click
+
+        Try
+            If cbo_Month.Text = "" Then
+                MsgBox("Month is empty", vbCritical, "Failed")
+                cbo_Month.Focus()
+            ElseIf cbo_year.Text = "" Then
+                MsgBox("year is empty", vbCritical, "Failed")
+                cbo_year.Focus()
+            ElseIf cbo_Devision.Text = "" Then
+                MsgBox("devision is empty", vbCritical, "Failed")
+                cbo_Devision.Focus()
+                Exit Sub
+            End If
+
+            For i As Integer = 0 To Me.dgv_ImportDataAllowence.Rows.Count - 2
+                code = Me.dgv_ImportDataAllowence.Rows(i).Cells(0).Value.ToString()
+                description = Me.dgv_ImportDataAllowence.Rows(i).Cells(1).Value.ToString()
+                value = Me.dgv_ImportDataAllowence.Rows(i).Cells(2).Value.ToString()
+
+                Dim sqlInsert As String = "INSERT INTO [AN_SUMATRA].[dbo].[TM_tb_ImportDataAllowence] ([month],[year],[devision],[code],[description],[value]) VALUES "
+                sqlInsert = sqlInsert & " ('" & cbo_Month.Text.ToString & "'" & _
+                    ",'" & cbo_year.Text & "'" & _
+                    ",'" & cbo_Devision.Text & "'" & _
+                    ",'" & code & "'" & _
+                    ",'" & description & "'" & _
+                    ",'" & value & "')"
+
+                cn.Execute(sqlInsert)
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+        cn.Close()
+    End Sub
+
+    Private Sub frm_ImportDataAllowence_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState < FormWindowState.Minimized Then
+            dgv_ImportDataAllowence.Top = 180
+            dgv_ImportDataAllowence.Height = Me.Height - 290
+            dgv_ImportDataAllowence.Width = Me.Width - 130
+
+            btn_Save.Top = Me.Height - btn_Save.Height - 70
+            btn_Save.Left = 53
+
+            btn_Cancel.Top = Me.Height - btn_Save.Height - 70
+            btn_Cancel.Left = 143
+
+            btn_ImportFromExcel.Left = Me.Width - btn_ImportFromExcel.Width - 75
+
+
+        Else
+            dgv_ImportDataAllowence.Top = 180
+            dgv_ImportDataAllowence.Height = Me.Height - 290
+            dgv_ImportDataAllowence.Width = Me.Width - 134
+
+            btn_Save.Top = Me.Height - btn_Save.Height - 70
+            btn_Save.Left = 53
+
+            btn_Cancel.Top = Me.Height - btn_Save.Height - 70
+            btn_Cancel.Left = 143
+
+            btn_ImportFromExcel.Left = Me.Width - btn_ImportFromExcel.Width - 80
+
+        End If
+
+    End Sub
+
+    Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
+        Me.Close()
+    End Sub
+End Class
