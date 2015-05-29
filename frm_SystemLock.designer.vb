7f5d12fc086fd8dfff1b533d8bc7c26e1e7ff90b
riza <rizayusuf@rocketmail.com>
2015-05-29 08:34:51 +0700

    form

---
+++ frm_SystemLock.designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1726")> _
+Partial Class frm_SystemLock
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_SystemLock))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btn_Exit = New System.Windows.Forms.Button()
+        Me.btn_Unlock = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lbl_CurrentAccountName = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt_Password = New System.Windows.Forms.TextBox()
+        Me.txt_Username = New System.Windows.Forms.TextBox()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.WindowText
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(435, 151)
+        Me.Panel1.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(245, 34)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(166, 105)
+        Me.PictureBox1.TabIndex = 18
+        Me.PictureBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(8, 34)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(158, 17)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "CAN UNLOCK THIS SYSTEM"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.Desktop
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(8, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Label3.Size = New System.Drawing.Size(326, 17)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "ONLY CURRENT USER OR USER WITH HIGH LEVEL PRIORITY"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.btn_Exit)
+        Me.Panel2.Controls.Add(Me.btn_Unlock)
+        Me.Panel2.Location = New System.Drawing.Point(0, 279)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(435, 36)
+        Me.Panel2.TabIndex = 32
+        '
+        'btn_Exit
+        '
+        Me.btn_Exit.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn_Exit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btn_Exit.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.btn_Exit.Location = New System.Drawing.Point(223, 4)
+        Me.btn_Exit.Name = "btn_Exit"
+        Me.btn_Exit.Size = New System.Drawing.Size(70, 26)
+        Me.btn_Exit.TabIndex = 17
+        Me.btn_Exit.Text = "Exit"
+        Me.btn_Exit.UseVisualStyleBackColor = False
+        '
+        'btn_Unlock
+        '
+        Me.btn_Unlock.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn_Unlock.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.btn_Unlock.Location = New System.Drawing.Point(147, 4)
+        Me.btn_Unlock.Name = "btn_Unlock"
+        Me.btn_Unlock.Size = New System.Drawing.Size(70, 26)
+        Me.btn_Unlock.TabIndex = 16
+        Me.btn_Unlock.Text = "Unlock"
+        Me.btn_Unlock.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lbl_CurrentAccountName)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.txt_Password)
+        Me.GroupBox1.Controls.Add(Me.txt_Username)
+        Me.GroupBox1.Controls.Add(Me.PasswordLabel)
+        Me.GroupBox1.Controls.Add(Me.UsernameLabel)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 173)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(410, 87)
+        Me.GroupBox1.TabIndex = 33
+        Me.GroupBox1.TabStop = False
+        '
+        'lbl_CurrentAccountName
+        '
+        Me.lbl_CurrentAccountName.AutoSize = True
+        Me.lbl_CurrentAccountName.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.lbl_CurrentAccountName.Location = New System.Drawing.Point(109, 8)
+        Me.lbl_CurrentAccountName.Name = "lbl_CurrentAccountName"
+        Me.lbl_CurrentAccountName.Size = New System.Drawing.Size(130, 15)
+        Me.lbl_CurrentAccountName.TabIndex = 38
+        Me.lbl_CurrentAccountName.Text = "Current Account Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Label1.Location = New System.Drawing.Point(14, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 15)
+        Me.Label1.TabIndex = 37
+        Me.Label1.Text = "Current User is :"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.Button1.Location = New System.Drawing.Point(355, 56)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(41, 20)
+        Me.Button1.TabIndex = 36
+        Me.Button1.Text = "<>"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'txt_Password
+        '
+        Me.txt_Password.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.txt_Password.Location = New System.Drawing.Point(112, 58)
+        Me.txt_Password.MaxLength = 50
+        Me.txt_Password.Name = "txt_Password"
+        Me.txt_Password.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt_Password.Size = New System.Drawing.Size(240, 20)
+        Me.txt_Password.TabIndex = 35
+        '
+        'txt_Username
+        '
+        Me.txt_Username.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.txt_Username.Location = New System.Drawing.Point(112, 32)
+        Me.txt_Username.MaxLength = 50
+        Me.txt_Username.Name = "txt_Username"
+        Me.txt_Username.Size = New System.Drawing.Size(240, 20)
+        Me.txt_Username.TabIndex = 33
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.PasswordLabel.Location = New System.Drawing.Point(14, 55)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(78, 23)
+        Me.PasswordLabel.TabIndex = 34
+        Me.PasswordLabel.Text = "&Password :"
+        Me.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.UsernameLabel.Location = New System.Drawing.Point(14, 28)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(78, 23)
+        Me.UsernameLabel.TabIndex = 32
+        Me.UsernameLabel.Text = "&User name :"
+        Me.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'frm_SystemLock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(434, 312)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frm_SystemLock"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "System Lock"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents btn_Exit As System.Windows.Forms.Button
+    Friend WithEvents btn_Unlock As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lbl_CurrentAccountName As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txt_Password As System.Windows.Forms.TextBox
+    Friend WithEvents txt_Username As System.Windows.Forms.TextBox
+    Friend WithEvents PasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents UsernameLabel As System.Windows.Forms.Label
+
+End Class
---
+++ frm_SystemLock.vb
@@ -0,0 +1,57 @@
+﻿Imports ADODB
+Public Class frm_SystemLock
+    Dim cn As New ADODB.Connection
+    Dim rs As New ADODB.Recordset
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button1_MouseDown1(sender As Object, e As MouseEventArgs) Handles Button1.MouseDown
+        txt_Password.PasswordChar = ""
+    End Sub
+
+    Private Sub Button1_MouseUp(sender As Object, e As MouseEventArgs) Handles Button1.MouseUp
+        txt_Password.PasswordChar = "*"
+    End Sub
+
+    Private Sub btn_Exit_Click(sender As Object, e As EventArgs) Handles btn_Exit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btn_Unlock_Click(sender As Object, e As EventArgs) Handles btn_Unlock.Click
+        Try
+            cn.ConnectionString = "Provider=SQLNCLI11;Server=192.168.0.1;Database=AN_SUMATRA;Uid=itdevelopment;Pwd=itdevelopment2015"
+            cn.Open()
+            If txt_Username.Text = "" Then
+                MsgBox("Username is Empty", vbInformation, "Unlock")
+                txt_Username.Focus()
+            ElseIf txt_Password.Text = "" Then
+                MsgBox("Password is Empty", vbInformation, "Unlock")
+                txt_Password.Focus()
+            Else
+                rs = New ADODB.Recordset
+                rs.Open("SELECT * FROM [AN_SUMATRA].[dbo].[SY_tb_appsaccounts] WHERE [username]='" & txt_Username.Text & "' and [password]='" & txt_Password.Text & "' and [priority] <= '" & CurrentAccountPriority & "' ", cn, CursorTypeEnum.adOpenKeyset)
+                If Not rs.EOF Then
+                    Me.Close()
+                Else
+                    MsgBox("Try Again", vbInformation, "Information")
+                    txt_Username.Text = ""
+                    txt_Password.Text = ""
+                    txt_Username.Focus()
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+        cn.Close()
+    End Sub
+
+    Private Sub frm_SystemLock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbl_CurrentAccountName.Text = CurrentAccountName
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        txt_Username.Focus()
+    End Sub
+End Class
