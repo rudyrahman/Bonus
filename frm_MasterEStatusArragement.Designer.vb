37022df282a3319a407d9759aa2db83c82fd7246
riza <rizayusuf@rocketmail.com>
2015-06-04 16:12:09 +0700

    Form master employee status arragement

---
+++ frm_MasterEStatusArragement.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_MasterEStatusArragement
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnl_Form = New System.Windows.Forms.Panel()
+        Me.btn_Close = New System.Windows.Forms.Button()
+        Me.btn_ExportToExcel = New System.Windows.Forms.Button()
+        Me.btn_RefreshData = New System.Windows.Forms.Button()
+        Me.btn_TambahBaru = New System.Windows.Forms.Button()
+        Me.txt_CariData = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.dgv_MasterEStatusArragement = New System.Windows.Forms.DataGridView()
+        Me.pnl_Form.SuspendLayout()
+        CType(Me.dgv_MasterEStatusArragement, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnl_Form
+        '
+        Me.pnl_Form.BackColor = System.Drawing.Color.Lavender
+        Me.pnl_Form.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnl_Form.Controls.Add(Me.btn_Close)
+        Me.pnl_Form.Controls.Add(Me.btn_ExportToExcel)
+        Me.pnl_Form.Controls.Add(Me.btn_RefreshData)
+        Me.pnl_Form.Controls.Add(Me.btn_TambahBaru)
+        Me.pnl_Form.Controls.Add(Me.txt_CariData)
+        Me.pnl_Form.Controls.Add(Me.Label1)
+        Me.pnl_Form.Controls.Add(Me.dgv_MasterEStatusArragement)
+        Me.pnl_Form.Location = New System.Drawing.Point(2, 2)
+        Me.pnl_Form.Name = "pnl_Form"
+        Me.pnl_Form.Size = New System.Drawing.Size(781, 459)
+        Me.pnl_Form.TabIndex = 8
+        '
+        'btn_Close
+        '
+        Me.btn_Close.Location = New System.Drawing.Point(667, 420)
+        Me.btn_Close.Name = "btn_Close"
+        Me.btn_Close.Size = New System.Drawing.Size(103, 27)
+        Me.btn_Close.TabIndex = 5
+        Me.btn_Close.Text = "Close"
+        Me.btn_Close.UseVisualStyleBackColor = True
+        '
+        'btn_ExportToExcel
+        '
+        Me.btn_ExportToExcel.Location = New System.Drawing.Point(340, 420)
+        Me.btn_ExportToExcel.Name = "btn_ExportToExcel"
+        Me.btn_ExportToExcel.Size = New System.Drawing.Size(103, 27)
+        Me.btn_ExportToExcel.TabIndex = 2
+        Me.btn_ExportToExcel.Text = "Export To Excel"
+        Me.btn_ExportToExcel.UseVisualStyleBackColor = True
+        '
+        'btn_RefreshData
+        '
+        Me.btn_RefreshData.Location = New System.Drawing.Point(449, 420)
+        Me.btn_RefreshData.Name = "btn_RefreshData"
+        Me.btn_RefreshData.Size = New System.Drawing.Size(103, 27)
+        Me.btn_RefreshData.TabIndex = 3
+        Me.btn_RefreshData.Text = "Refresh Data"
+        Me.btn_RefreshData.UseVisualStyleBackColor = True
+        '
+        'btn_TambahBaru
+        '
+        Me.btn_TambahBaru.Location = New System.Drawing.Point(558, 420)
+        Me.btn_TambahBaru.Name = "btn_TambahBaru"
+        Me.btn_TambahBaru.Size = New System.Drawing.Size(103, 27)
+        Me.btn_TambahBaru.TabIndex = 4
+        Me.btn_TambahBaru.Text = "Tambah Baru"
+        Me.btn_TambahBaru.UseVisualStyleBackColor = True
+        '
+        'txt_CariData
+        '
+        Me.txt_CariData.Location = New System.Drawing.Point(76, 20)
+        Me.txt_CariData.MaxLength = 100
+        Me.txt_CariData.Name = "txt_CariData"
+        Me.txt_CariData.Size = New System.Drawing.Size(374, 20)
+        Me.txt_CariData.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 22
+        Me.Label1.Text = "Cari Data :"
+        '
+        'dgv_MasterEStatusArragement
+        '
+        Me.dgv_MasterEStatusArragement.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.dgv_MasterEStatusArragement.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_MasterEStatusArragement.Location = New System.Drawing.Point(9, 66)
+        Me.dgv_MasterEStatusArragement.Name = "dgv_MasterEStatusArragement"
+        Me.dgv_MasterEStatusArragement.Size = New System.Drawing.Size(761, 325)
+        Me.dgv_MasterEStatusArragement.TabIndex = 21
+        Me.dgv_MasterEStatusArragement.TabStop = False
+        '
+        'frm_MasterEStatusArragement
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 462)
+        Me.Controls.Add(Me.pnl_Form)
+        Me.Name = "frm_MasterEStatusArragement"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "frm_MasterEStatusArragement"
+        Me.pnl_Form.ResumeLayout(False)
+        Me.pnl_Form.PerformLayout()
+        CType(Me.dgv_MasterEStatusArragement, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnl_Form As System.Windows.Forms.Panel
+    Friend WithEvents btn_Close As System.Windows.Forms.Button
+    Friend WithEvents btn_ExportToExcel As System.Windows.Forms.Button
+    Friend WithEvents btn_RefreshData As System.Windows.Forms.Button
+    Friend WithEvents btn_TambahBaru As System.Windows.Forms.Button
+    Friend WithEvents txt_CariData As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents dgv_MasterEStatusArragement As System.Windows.Forms.DataGridView
+End Class
---
+++ frm_MasterEStatusArragement.vb
@@ -0,0 +1,169 @@
+﻿Imports ADODB
+Imports vbexcel = Microsoft.Office.Interop.Excel
+Imports System.Data.OleDb
+Public Class frm_MasterEStatusArragement
+    Dim cn As New ADODB.Connection
+    Dim rs As New ADODB.Recordset
+    Dim strs As String
+    Dim search As String
+
+    Dim rsExcel As ADODB.Recordset
+    Dim strExcel As String
+    Private Excel03ConString As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'"
+    Private Excel07ConString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'"
+    Private Sub frm_MasterEStatusArragement_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        frm_MasterEStatusArragement_Resize(Me, Nothing)
+        txt_CariData.Focus()
+        Try
+            cn.ConnectionString = "Provider=SQLNCLI11;Server=192.168.0.1;Database=AN_SUMATRA;Uid=itdevelopment;Pwd=itdevelopment2015"
+            cn.Open()
+            rs = New ADODB.Recordset
+            strs = "SELECT * FROM  [AN_SUMATRA].[dbo].[TM_tb_statusarragement] order by [id]"
+
+            With rs
+                .CursorLocation = CursorLocationEnum.adUseClient
+                .Open(strs, cn, CursorTypeEnum.adOpenKeyset, _
+                      LockTypeEnum.adLockReadOnly)
+                .ActiveConnection = Nothing
+            End With
+            Me.dgv_MasterEStatusArragement.DataSource = RecordSetToDataTable(rs)
+            Call tampilgrid()
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+
+    End Sub
+    Sub tampilgrid()
+        dgv_MasterEStatusArragement.Columns(0).Width = 40
+        dgv_MasterEStatusArragement.Columns(1).Width = 100
+        dgv_MasterEStatusArragement.Columns(2).Width = 150
+        dgv_MasterEStatusArragement.Columns(3).Width = 150
+        dgv_MasterEStatusArragement.Columns(4).Width = 100
+        dgv_MasterEStatusArragement.Columns(5).Width = 70
+        dgv_MasterEStatusArragement.Columns(6).Width = 60
+        dgv_MasterEStatusArragement.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None
+        dgv_MasterEStatusArragement.RowTemplate.Height = 17
+        dgv_MasterEStatusArragement.Columns(5).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+        dgv_MasterEStatusArragement.RowsDefaultCellStyle.BackColor = Color.Lavender
+        dgv_MasterEStatusArragement.AlternatingRowsDefaultCellStyle.BackColor = Color.White
+    End Sub
+    Public Function RecordSetToDataTable( _
+             ByVal objRS As ADODB.Recordset) As DataTable
+
+        Dim objDA As New OleDbDataAdapter()
+        Dim objDT As New DataTable()
+        objDA.Fill(objDT, objRS)
+        Return objDT
+
+    End Function
+    Private Sub frm_MasterEStatusArragement_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.Height > 300 Then
+            pnl_Form.Height = Me.Height - (pnl_Form.Top * 2) - 40
+            btn_ExportToExcel.Top = pnl_Form.Height - pnl_Form.Top - btn_ExportToExcel.Height - 10
+            btn_RefreshData.Top = pnl_Form.Height - pnl_Form.Top - btn_RefreshData.Height - 10
+            btn_TambahBaru.Top = pnl_Form.Height - pnl_Form.Top - btn_TambahBaru.Height - 10
+            btn_Close.Top = pnl_Form.Height - pnl_Form.Top - btn_Close.Height - 10
+
+            dgv_MasterEStatusArragement.Top = 66
+            dgv_MasterEStatusArragement.Height = Me.Height - 180
+
+        End If
+        If Me.Width > 570 Then
+            pnl_Form.Width = Me.Width - (pnl_Form.Left * 2) - 15
+            btn_Close.Left = Me.Width - btn_Close.Width - 30
+            btn_TambahBaru.Left = Me.Width - btn_TambahBaru.Width - 138
+            btn_RefreshData.Left = Me.Width - btn_RefreshData.Width - 246
+            btn_ExportToExcel.Left = Me.Width - btn_ExportToExcel.Width - 354
+            dgv_MasterEStatusArragement.Width = Me.Width - 40
+
+        End If
+
+    End Sub
+    Private Sub btn_TambahBaru_Click_1(sender As Object, e As EventArgs) Handles btn_TambahBaru.Click
+        frm_MasterEStatusArragementAddNew.ShowDialog()
+    End Sub
+
+    Private Sub btn_Close_Click(sender As Object, e As EventArgs) Handles btn_Close.Click
+        Me.Close()
+        cn.Close()
+    End Sub
+
+    Private Sub btn_RefreshData_Click(sender As Object, e As EventArgs) Handles btn_RefreshData.Click
+        Try
+            rs = New ADODB.Recordset
+            strs = "SELECT * FROM  [AN_SUMATRA].[dbo].[TM_tb_statusarragement] order by [id]"
+
+            With rs
+                .CursorLocation = CursorLocationEnum.adUseClient
+                .Open(strs, cn, CursorTypeEnum.adOpenKeyset, _
+                      LockTypeEnum.adLockReadOnly)
+                .ActiveConnection = Nothing
+            End With
+            Me.dgv_MasterEStatusArragement.DataSource = RecordSetToDataTable(rs)
+            Call tampilgrid()
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+    End Sub
+
+    Private Sub btn_ExportToExcel_Click(sender As Object, e As EventArgs) Handles btn_ExportToExcel.Click
+        Try
+            Dim excelapp As vbexcel.Application
+            Dim excelworkbook As vbexcel.Workbook
+            Dim excelworksheet As vbexcel.Worksheet
+            Dim msvalue As Object = System.Reflection.Missing.Value
+
+            excelapp = New vbexcel.Application
+            excelworkbook = excelapp.Workbooks.Add(msvalue)
+            excelworksheet = excelworkbook.Sheets("sheet1")
+
+            'Perulangan untuk memindahkan data dari datagridview ke worksheet excel
+            For j = 0 To dgv_MasterEStatusArragement.Columns.Count - 1
+
+                excelworksheet.Cells(1, j + 1) = dgv_MasterEStatusArragement.Columns(j).HeaderText
+                For i = 1 To dgv_MasterEStatusArragement.Rows.Count - 1
+                    '
+                    excelworksheet.Cells(i + 1, j + 1) = "'" & dgv_MasterEStatusArragement(j, i - 1).Value.ToString
+                Next
+
+            Next
+
+            ' Mengatur ukuran baris dan column
+            excelworksheet.UsedRange.EntireRow.AutoFit()
+            excelworksheet.UsedRange.EntireColumn.AutoFit()
+
+            ' Mengatur border table
+            excelworksheet.UsedRange.Borders.LineStyle = vbexcel.XlLineStyle.xlContinuous
+            excelworksheet.UsedRange.Borders.Color = Color.Black
+            excelworksheet.UsedRange.Borders.Weight = vbexcel.XlBorderWeight.xlThin
+            excelapp.Visible = True
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+    End Sub
+
+    Private Sub txt_CariData_TextChanged(sender As Object, e As EventArgs) Handles txt_CariData.TextChanged
+        Try
+            rs = New ADODB.Recordset
+            search = "select * from [AN_SUMATRA].[dbo].[TM_tb_statusarragement] where [description] like '%" & txt_CariData.Text & "%' order by [id]"
+            With rs
+                .CursorLocation = CursorLocationEnum.adUseClient
+                .Open(search, cn, CursorTypeEnum.adOpenKeyset, _
+                      LockTypeEnum.adLockReadOnly)
+                .ActiveConnection = Nothing
+            End With
+            Me.dgv_MasterEStatusArragement.DataSource = RecordSetToDataTable(rs)
+            Call tampilgrid()
+            If rs.BOF Then
+                dgv_MasterEStatusArragement.DataSource = rs
+                dgv_MasterEStatusArragement.Refresh()
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
+
+    End Sub
+
+End Class
