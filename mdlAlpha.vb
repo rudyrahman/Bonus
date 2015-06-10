8e59cdcd0c6f9052f4a3af8786062ce6aefc59e1
riza <rizayusuf@rocketmail.com>
2015-06-10 08:14:17 +0700

    tambah module koneksi

--- mdlAlpha.vb
+++ mdlAlpha.vb
@@ -1,6 +1,16 @@
-﻿Module mdlAlpha
+﻿Imports ADODB
+Imports System.Data.OleDb
+Module mdlAlpha
+    Dim cn As New ADODB.Connection
+    Dim rs As New ADODB.Recordset
+
     Public mainconnectionstring As String
     Public CurrentAccountId As String
     Public CurrentAccountName As String
     Public CurrentAccountPriority As Byte
+
+    Sub connect()
+        cn.ConnectionString = "Provider=SQLNCLI11;Server=192.168.0.1;Database=AN_SUMATRA;Uid=itdevelopment;Pwd=itdevelopment2015"
+        cn.Open()
+    End Sub
 End Module
