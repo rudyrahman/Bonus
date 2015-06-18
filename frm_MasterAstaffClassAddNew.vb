71ecc80ac1e973b9be4ebce37698a0b51220e891
riza <rizayusuf@rocketmail.com>
2015-06-18 08:36:30 +0700

    form master staff class

--- frm_MasterAstaffClassAddNew.vb
+++ frm_MasterAstaffClassAddNew.vb
@@ -11,11 +11,11 @@ Public Class frm_MasterAstaffClassAddNew
             Dim Hitung As Long
             With rs
                 If .EOF Then
-                    Urutan = "SC0" + "001"
+                    Urutan = "SC" + "001"
                     txt_code.Text = Urutan
                 Else
                     Hitung = Val(Microsoft.VisualBasic.Strings.Right(rs(0).Value.ToString, 3)) + 1
-                    Urutan = "SC0" + Microsoft.VisualBasic.Strings.Right("000" & Hitung, 3)
+                    Urutan = "SC" + Microsoft.VisualBasic.Strings.Right("000" & Hitung, 3)
                 End If
                 txt_code.Text = Urutan
 
@@ -34,5 +34,22 @@ Public Class frm_MasterAstaffClassAddNew
 
     Private Sub btn_Cancel_Click(sender As Object, e As EventArgs) Handles btn_Cancel.Click
         Me.Close()
+        cn.Close()
+    End Sub
+
+    Private Sub btn_AddNew_Click(sender As Object, e As EventArgs) Handles btn_AddNew.Click
+        Try
+            If txt_Code.Text = "" Or txt_Description.Text = "" Then
+                MsgBox(udv_msg_LoginBlank, vbInformation)
+            Else
+                Dim sqlInsert As String = "INSERT INTO [AN_SUMATRA].[dbo].[TM_tb_staffclass] ([code],[description],[create_by],[create_time],[system_id],[inactive]) VALUES "
+                sqlInsert = sqlInsert & " ('" & txt_Code.Text.ToString & "','" & txt_Description.Text & "','" & CurrentAccountName & "'," & "GETDATE()" & " " & ",'" & CurrentAccountId & "','N')"
+                cn.Execute(sqlInsert)
+                txt_Code.Text = ""
+                txt_Description.Text = ""
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical)
+        End Try
     End Sub
 End Class
